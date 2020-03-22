4970f6608dd17de73d4b1add6bb54eb183bd93e0
Francisco José Romero Medina <f.romeromedina@gmail.com>
2020-03-22 18:17:45 +0100

    Añadir testing

--- cfg/scripts/vars.sh
+++ cfg/scripts/vars.sh
@@ -5,8 +5,9 @@ host='root@192.168.0.100'
 web_host='root@romeronet.es'
 project_name='kazoku'
 web_root="/root/${project_name}"
-project_name_testing='kazoku'
-web_root_testing="/root/${project_name}"
 project_name_upper='KAZOKU'
 url='kazoku.romeronet.es'
+
+project_name_testing="${project_name}_testing"
+web_root_testing="/root/${project_name_testing}"
 url_testing='testing_kazoku.romeronet.es'
