9b9fd2be13d4af426ee93b0ce7be61faec0d7176
Francisco José Romero Medina <f.romeromedina@gmail.com>
2020-03-22 18:12:44 +0100

    Añadir testing

---
+++ cfg/scripts/generate-env-testing.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source $(dirname $0)/vars.sh
+
+rm -rf .env
+touch .env
+echo "${project_name_upper}_DATABASE_USER=${DATABASE_DATA_TESTING_USR}" >> .env
+echo "${project_name_upper}_DATABASE_PASSWORD=${DATABASE_DATA_TESTING_PSW}" >> .env
+echo "${project_name_upper}_DATABASE_DATABASE=${DATABASE_DATA_DBNAME}" >> .env
+
+ssh ${host} "mkdir ${web_root_testing} -p"
+scp .env ${host}:${web_root_testing}/.env
+scp ./docker-compose-testing.yml ${host}:${web_root_testing}/docker-compose.yml
+rm -rf .env
