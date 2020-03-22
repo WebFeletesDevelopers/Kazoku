d9b7b616b8b599cb30f4d2b87933780f9c5c721f
Francisco José Romero Medina <f.romeromedina@gmail.com>
2020-03-22 15:37:59 +0100

    Arreglar host de archivos de nginx

--- cfg/scripts/deploy.sh
+++ cfg/scripts/deploy.sh
@@ -5,8 +5,8 @@ source $(dirname $0)/vars.sh
 ssh ${host} "docker pull ${registry_url}/${project_name}-php && docker pull ${registry_url}/${project_name}-web"
 ssh ${host} "cd ${web_root} && docker-compose up -d"
 
-scp ./cfg/nginx_real_settings.conf ${host}:/etc/nginx/sites-enabled/${url}.conf
-ssh ${host} "systemctl restart nginx"
+scp ./cfg/nginx_real_settings.conf ${web_host}:/etc/nginx/sites-enabled/${url}.conf
+ssh ${web_host} "systemctl restart nginx"
 
 echo 'Despliegue completado en romeronet con la(s) URL:'
 echo ${url}
--- cfg/scripts/vars.sh
+++ cfg/scripts/vars.sh
@@ -2,6 +2,8 @@
 
 registry_url='registry.romeronet.es'
 host='root@192.168.0.100'
+web_host='root@romeronet.es'
 project_name='kazoku'
 web_root="/root/${project_name}"
 project_name_upper='KAZOKU'
+url=kazoku.romeronet.es
