a38f8df508e3151624c5448bc7b2eccc77c959f9
thaana <albertogomp@gmail.com>
2020-05-30 14:00:15 +0200

    Added views and actions of several pages. Added routes

--- src/Routes.php
+++ src/Routes.php
@@ -3,8 +3,16 @@
 namespace WebFeletesDevelopers\Kazoku;
 
 use Slim\App;
+use WebFeletesDevelopers\Kazoku\Action\assistanceAction;
 use WebFeletesDevelopers\Kazoku\Action\HomeAction;
+use WebFeletesDevelopers\Kazoku\Action\judokasAction;
+use WebFeletesDevelopers\Kazoku\Action\myClassAction;
+use WebFeletesDevelopers\Kazoku\Action\newUserAction;
+use WebFeletesDevelopers\Kazoku\Action\panelAction;
+use WebFeletesDevelopers\Kazoku\Action\profileAction;
 use WebFeletesDevelopers\Kazoku\Action\PruebaTraduccionAction;
+use WebFeletesDevelopers\Kazoku\Action\userCheckAction;
+use WebFeletesDevelopers\Kazoku\Action\virtualClassAction;
 
 /**
  * Class Routes
@@ -20,6 +28,14 @@ class Routes
     {
         $app->get('/', HomeAction::class);
         $app->get('/pruebatraduccion', PruebaTraduccionAction::class);
+        $app->get('/profile', profileAction::class);
+        $app->get('/userCheck', userCheckAction::class);
+        $app->get('/judokas', judokasAction::class);
+        $app->get('/assistance', assistanceAction::class);
+        $app->get('/myClass', myClassAction::class);
+        $app->get('/virtualClass', virtualClassAction::class);
+        $app->get('/newUser', newUserAction::class);
+        $app->get('/panel', panelAction::class);
         return $app;
     }
 }
