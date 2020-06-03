b9adb8602765b27f59d6fb94fb2f6d9e1e185b36
thaana <albertogomp@gmail.com>
2020-06-03 10:30:05 +0200

    Ended newUserTemplate Added some translations

--- src/Action/newUserAction.php
+++ src/Action/newUserAction.php
@@ -21,7 +21,9 @@ class newUserAction extends BaseTwigAction implements ActionInterface
 {
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args = []): ResponseInterface
     {
-
+        setlocale(LC_ALL, 'en_US.UTF-8');
+        bindtextdomain('kazoku', __DIR__ . '/../locale');
+        textdomain('kazoku');
         $body = $response->getBody();
         //$compiledTwig = $this->render('home');
         $compiledTwig = $this->render('newUser',['title' => "titulo",'userName' => "Alberto",'title' => "titulo",'userId' => 0]);
