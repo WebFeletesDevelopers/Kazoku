556d01f91c7696e5ec0755056f07ab664d48d419
Fran Romero <f.romeromedina@gmail.com>
2020-06-06 16:58:26 +0200

    Add responsive and feedback to login

--- web/ts/login/LoginMain.ts
+++ web/ts/login/LoginMain.ts
@@ -12,7 +12,8 @@ export class LoginMain {
     }
 
     private static handleLoginForm(): void {
-        const formElement: HTMLFormElement = document.querySelector('div.login-form form');
+        const loginContainer: HTMLDivElement = document.querySelector('div.login-form')
+        const formElement: HTMLFormElement = loginContainer.querySelector('form');
         const userElement: HTMLInputElement = formElement.querySelector('input[name="username"]');
         const passwordElement: HTMLInputElement = formElement.querySelector('input[name="password"]');
         const loginButton: HTMLButtonElement = formElement.querySelector('button');
@@ -22,26 +23,40 @@ export class LoginMain {
             passwordElement.value
         );
 
+        loginContainer.addEventListener('animationend', e => {
+            (e.target as HTMLDivElement).classList.remove('animate');
+        });
+
         userElement.addEventListener('keyup', () => {
             loginData.username = userElement.value;
+            loginContainer.classList.remove('kazoku-error-shadow');
         });
         passwordElement.addEventListener('keyup', () => {
             loginData.password = passwordElement.value;
+            loginContainer.classList.remove('kazoku-error-shadow');
         })
 
         loginButton.addEventListener('click', e => {
             e.preventDefault();
-            LoginMain.login(loginData);
+            (e.target as HTMLButtonElement).disabled = true;
+            LoginMain.login(loginData, loginContainer, loginButton);
         });
     }
 
-    private static login(loginData: Login) {
+    private static login(
+        loginData: Login,
+        loginContainer: HTMLDivElement,
+        loginButton: HTMLButtonElement
+    ) {
         LoginRequest.getLoginHash(loginData).then(res => {
             if (res.statusCode === 200) {
                 const hash = res.message['hash']
                 Cookie.set('hash', hash);
                 document.location.replace('/');
+            } else {
+                loginContainer.classList.add('kazoku-error-shadow', 'animate');
             }
+            loginButton.disabled = false;
         });
     }
 }
