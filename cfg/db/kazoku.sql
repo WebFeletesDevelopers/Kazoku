067ed1f186c4d50f19626bc2ec3d80e563fe4151
Fran Romero <f.romeromedina@gmail.com>
2020-06-09 22:55:35 +0200

    Modify database

--- cfg/db/kazoku.sql
+++ cfg/db/kazoku.sql
@@ -30,24 +30,30 @@ USE kazoku;
 -- Table structure for table `alumno`
 --
 
-CREATE TABLE `alumno` (
-  `Nombre` varchar(50) CHARACTER SET utf8 NOT NULL DEFAULT 'Nombre',
-  `Apellido1` varchar(50) CHARACTER SET utf8 NOT NULL DEFAULT 'Apellido',
-  `Apellido2` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
-  `Sexo` tinyint(1) NOT NULL DEFAULT 1,
-  `CodAlumno` int(11) NOT NULL,
-  `CodUsu` int(11) DEFAULT NULL,
-  `IdFanjyda` int(5) DEFAULT NULL,
-  `DNI` varchar(9) CHARACTER SET utf8 DEFAULT NULL,
-  `FechaNacimiento` date DEFAULT NULL,
-  `Telefono` int(11) DEFAULT NULL,
-  `Email` varchar(22) CHARACTER SET utf8 DEFAULT NULL,
-  `Enfermedad` longtext CHARACTER SET utf8 DEFAULT NULL,
-  `CodTut` int(11) DEFAULT NULL,
-  `CodCinturon` int(11) DEFAULT 1,
-  `CodDireccion` int(11) DEFAULT NULL,
-  `CodClase` int(11) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+CREATE TABLE `pupil` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `pupil_id` int(11) NOT NULL,
+    `name` varchar(64) CHARACTER SET utf8 NOT NULL DEFAULT 'Nombre',
+    `surname` varchar(64) CHARACTER SET utf8 NOT NULL DEFAULT 'Apellido',
+    `second_surname` varchar(64) CHARACTER SET utf8 DEFAULT NULL,
+    `gender` tinyint(1) NOT NULL DEFAULT 1,
+    `fanjyda_id` int(11) DEFAULT NULL,
+    `DNI` varchar(9) CHARACTER SET utf8 DEFAULT NULL,
+    `birthDate` date DEFAULT NULL,
+    `phone` int(11) DEFAULT NULL,
+    `email` varchar(22) CHARACTER SET utf8 DEFAULT NULL,
+    `extra_info` longtext CHARACTER SET utf8 DEFAULT NULL,
+    `trainer_id` int(11) DEFAULT NULL,
+    `belt_id` int(11) DEFAULT 1,
+    `address_id` int(11) DEFAULT NULL,
+    `class_id` int(11) DEFAULT NULL,
+    PRIMARY KEY (pupil_id),
+    UNIQUE (DNI),
+    FOREIGN KEY (trainer_id) REFERENCES trainer(id),
+    FOREIGN KEY (belt_id) REFERENCES belt(id),
+    FOREIGN KEY (address_id) REFERENCES address(id),
+    FOREIGN KEY (class_id) REFERENCES class(id)
+) CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -55,13 +61,14 @@ CREATE TABLE `alumno` (
 -- Table structure for table `centro`
 --
 
-CREATE TABLE `centro` (
-  `CodCentro` int(11) NOT NULL,
-  `Nombre` varchar(100) NOT NULL,
-  `Direccion` longtext DEFAULT NULL,
-  `CodPostal` int(5) DEFAULT NULL,
-  `Telefono` int(9) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `center` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) NOT NULL,
+    `address_id` INT(11) DEFAULT NULL,
+    `phone` int(9) DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (address_id) REFERENCES address(id)
+) CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -69,10 +76,11 @@ CREATE TABLE `centro` (
 -- Table structure for table `cinturon`
 --
 
-CREATE TABLE `cinturon` (
-  `CodCinturon` int(11) NOT NULL,
-  `Cinturon` varchar(50) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `belt` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(64) NOT NULL,
+    PRIMARY KEY (id)
+) CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -80,16 +88,18 @@ CREATE TABLE `cinturon` (
 -- Table structure for table `clase`
 --
 
-CREATE TABLE `clase` (
-  `CodClase` int(11) NOT NULL,
-  `Horario` varchar(15) NOT NULL,
-  `Profesor` varchar(20) NOT NULL,
-  `EdadMin` int(11) DEFAULT NULL,
-  `EdadMax` int(11) DEFAULT NULL,
-  `Nombre` varchar(200) DEFAULT NULL,
-  `CodCentro` int(11) NOT NULL,
-  `Dias` int(3) NOT NULL DEFAULT 0
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `class` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `schedule` varchar(15) NOT NULL,
+    `trainer_id` varchar(20) NOT NULL,
+    `minimum_age` int(11) DEFAULT NULL,
+    `maximum_age` int(11) DEFAULT NULL,
+    `name` varchar(200) DEFAULT NULL,
+    `center_id` int(11) NOT NULL,
+    `days` int(3) NOT NULL DEFAULT 0,
+    PRIMARY KEY (id),
+    FOREIGN KEY (center_id) REFERENCES center(id)
+) CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -97,13 +107,14 @@ CREATE TABLE `clase` (
 -- Table structure for table `direccion`
 --
 
-CREATE TABLE `direccion` (
-  `CodDireccion` int(11) NOT NULL,
-  `CodigoPostal` int(11) NOT NULL,
-  `Localidad` varchar(50) NOT NULL,
-  `Provincia` varchar(50) NOT NULL,
-  `Direccion` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `address` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `zip_code` int(11) NOT NULL,
+    `locality` varchar(50) NOT NULL,
+    `province` varchar(50) NOT NULL,
+    `address` varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+) CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -111,14 +122,16 @@ CREATE TABLE `direccion` (
 -- Table structure for table `noticias`
 --
 
-CREATE TABLE `noticias` (
-  `CodNot` int(111) NOT NULL,
-  `Titulo` varchar(255) NOT NULL,
-  `Cuerpo` longtext NOT NULL,
-  `Fecha` date DEFAULT current_timestamp(),
-  `Autor` varchar(255) NOT NULL,
-  `Publica` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `news` (
+    `id` int(111) NOT NULL AUTO_INCREMENT,
+    `title` varchar(255) NOT NULL,
+    `body` longtext NOT NULL,
+    `date` date DEFAULT current_timestamp(),
+    `user_id` varchar(255) NOT NULL,
+    `public` tinyint(1) NOT NULL DEFAULT 0,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+) CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -126,16 +139,16 @@ CREATE TABLE `noticias` (
 -- Table structure for table `tutor`
 --
 
-CREATE TABLE `tutor` (
-  `CodTut` int(11) NOT NULL,
-  `Nombre` varchar(50) NOT NULL,
-  `Apellido1` varchar(50) NOT NULL,
-  `Apellido2` varchar(50) DEFAULT NULL,
-  `Telefono` int(11) DEFAULT NULL,
-  `DNI` varchar(9) DEFAULT NULL,
-  `CodDireccion` int(11) NOT NULL,
-  `Correo` varchar(255) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `trainer` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(50) NOT NULL,
+    `surname` varchar(50) NOT NULL,
+    `second_surname` varchar(50) DEFAULT NULL,
+    `phone` int(11) DEFAULT NULL,
+    `DNI` varchar(9) DEFAULT NULL,
+    `address_id` int(11) NOT NULL,
+    `email` varchar(255) DEFAULT NULL
+) CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -144,18 +157,19 @@ CREATE TABLE `tutor` (
 --
 
 CREATE TABLE `users` (
-  `Confirmado` tinyint(1) DEFAULT 0,
-  `Rango` int(1) NOT NULL DEFAULT 2,
-  `CodUsu` int(11) NOT NULL,
-  `username` varchar(80) NOT NULL,
-  `name` varchar(80) NOT NULL,
-  `Telefono` int(9) DEFAULT NULL,
-  `Apellido1` varchar(50) DEFAULT NULL,
-  `Apellido2` varchar(50) DEFAULT NULL,
-  `password` varchar(60) NOT NULL,
-  `Email` varchar(255) DEFAULT NULL,
-  `EmailConfirmado` tinyint(1) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `confirmed` tinyint(1) DEFAULT 0,
+    `rank` int(1) NOT NULL DEFAULT 2,
+    `username` varchar(80) NOT NULL,
+    `name` varchar(80) NOT NULL,
+    `phone` int(9) DEFAULT NULL,
+    `surname` varchar(50) DEFAULT NULL,
+    `second_surname` varchar(50) DEFAULT NULL,
+    `password` varchar(60) NOT NULL,
+    `email` varchar(255) DEFAULT NULL,
+    `email_confirmed` tinyint(1) DEFAULT 0,
+    PRIMARY KEY (id)
+) CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -163,171 +177,14 @@ CREATE TABLE `users` (
 -- Table structure for table `Verification`
 --
 
-CREATE TABLE `Verification` (
-  `Code` varchar(255) NOT NULL,
-  `CodKey` int(11) NOT NULL,
-  `Uname` varchar(50) NOT NULL,
-  `confirmado` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `verification` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `code` VARCHAR(64) NOT NULL,
+    `user_id` INT(11) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+) CHARSET=utf8 COLLATE=utf8_general_ci;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `alumno`
---
-ALTER TABLE `alumno`
-  ADD PRIMARY KEY (`CodAlumno`),
-  ADD UNIQUE KEY `DNI` (`DNI`),
-  ADD KEY `CodTut` (`CodTut`),
-  ADD KEY `CodCinturon` (`CodCinturon`),
-  ADD KEY `CodDireccion` (`CodDireccion`),
-  ADD KEY `CodClase` (`CodClase`);
-
---
--- Indexes for table `centro`
---
-ALTER TABLE `centro`
-  ADD PRIMARY KEY (`CodCentro`),
-  ADD UNIQUE KEY `Nombre` (`Nombre`);
-
---
--- Indexes for table `cinturon`
---
-ALTER TABLE `cinturon`
-  ADD PRIMARY KEY (`CodCinturon`);
-
---
--- Indexes for table `clase`
---
-ALTER TABLE `clase`
-  ADD PRIMARY KEY (`CodClase`),
-  ADD KEY `CodCentro` (`CodCentro`);
-
---
--- Indexes for table `direccion`
---
-ALTER TABLE `direccion`
-  ADD PRIMARY KEY (`CodDireccion`);
-
---
--- Indexes for table `noticias`
---
-ALTER TABLE `noticias`
-  ADD PRIMARY KEY (`CodNot`);
-
---
--- Indexes for table `tutor`
---
-ALTER TABLE `tutor`
-  ADD PRIMARY KEY (`CodTut`),
-  ADD KEY `CodDireccion` (`CodDireccion`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`CodUsu`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `Email` (`Email`),
-  ADD UNIQUE KEY `Telefono` (`Telefono`);
-
---
--- Indexes for table `Verification`
---
-ALTER TABLE `Verification`
-  ADD PRIMARY KEY (`CodKey`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `alumno`
---
-ALTER TABLE `alumno`
-  MODIFY `CodAlumno` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `centro`
---
-ALTER TABLE `centro`
-  MODIFY `CodCentro` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `cinturon`
---
-ALTER TABLE `cinturon`
-  MODIFY `CodCinturon` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `clase`
---
-ALTER TABLE `clase`
-  MODIFY `CodClase` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `direccion`
---
-ALTER TABLE `direccion`
-  MODIFY `CodDireccion` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `noticias`
---
-ALTER TABLE `noticias`
-  MODIFY `CodNot` int(111) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tutor`
---
-ALTER TABLE `tutor`
-  MODIFY `CodTut` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `CodUsu` int(11) NOT NULL AUTO_INCREMENT;
-
-
---
--- Table structure for table `todo`
---
-
-CREATE TABLE `todo` (
-                        `TaskCode` int(11) NOT NULL,
-                        `Name` varchar(100) DEFAULT NULL,
-                        `Progress` tinyint(3) NOT NULL DEFAULT 0,
-                        `Comments` text DEFAULT NULL,
-                        `DeathLine` date DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-
-INSERT INTO `todo` (`TaskCode`, `Name`, `Progress`, `Comments`, `DeathLine`) VALUES
-(33, 'Rediseño de interfaces', 80, '', NULL),
-(32, 'Gestión de cuentas', 76, '', NULL),
-(31, 'Conversión a PDO', 80, '', NULL),
-(37, 'mykolan', 101, 'Verificación para Antonio', NULL);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `todo`
---
-ALTER TABLE `todo`
-    ADD PRIMARY KEY (`TaskCode`);
-
-
---
--- AUTO_INCREMENT for table `Verification`
---
-ALTER TABLE `Verification`
-  MODIFY `CodKey` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
