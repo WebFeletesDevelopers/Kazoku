8d99d8cca72eb7fd76dc6e560fb11715091d64de
Francisco Jose Romero Medina <f.romeromedina@gmail.com>
2020-06-12 10:03:54 +0200

    Add language switcher

--- web/ts/general/GeneralMain.ts
+++ web/ts/general/GeneralMain.ts
@@ -3,9 +3,14 @@ import {Cookie} from "../util/Cookie";
 export class GeneralMain {
     public static handle(): void {
         const hasLogoutLink: boolean = !! document.querySelector('[data-action="logout-button"]');
+        const hasUpdateLanguage: boolean = !! document.querySelector('[data-action="change-language"]');
 
         if (hasLogoutLink) {
             GeneralMain.handleLogout();
+        }
+
+        if (hasUpdateLanguage) {
+            GeneralMain.handleUpdateLanguage();
         }
     }
 
@@ -16,6 +21,20 @@ export class GeneralMain {
             e.preventDefault();
             Cookie.delete('hash');
             document.location.replace('/');
+        });
+    }
+
+    private static handleUpdateLanguage(): void {
+        const updateLanguageLink: HTMLAnchorElement = document.querySelector('[data-action="change-language"]');
+
+        updateLanguageLink.addEventListener('click', e => {
+            e.preventDefault();
+            if (!! Cookie.get('lang')) {
+                Cookie.delete('lang');
+            } else {
+                Cookie.set('lang', '1');
+            }
+            document.location.reload();
         });
     }
 }
